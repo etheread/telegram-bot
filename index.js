4fbfc60d444adac21789cdb364e972386ac1128b
etheread <160173197+etheread@users.noreply.github.com>
2026-02-03 19:39:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Bot is running...');
-}).listen(process.env.PORT || 8080);
+}).listen(8000);
 
 require('dotenv').config()
 const TelegramAPI = require('node-telegram-bot-api')
